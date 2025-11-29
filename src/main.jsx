5ee99dff501646ecbaeb9ca807596eafc880faf5
dptosistemasumn-crypto <dptosistemasumn@gmail.com>
2025-11-28 21:28:45 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // (Si te da error esta línea, bórrala, pero intenta dejarla primero)
+// import './index.css'  <-- Si no tienes estilos globales, mantén esto comentado o bórralo
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
